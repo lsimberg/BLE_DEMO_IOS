2548db68569c1b5484ffdcf2b1d1794332f11146
Leo Simberg <lsimberg@izeni.com>
2014-09-10 11:58:57 -0600

    Initial Commit

---
+++ BluetoothLeo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothLeo1
+//
+//  Created by Leo Simberg on 9/10/14.
+//  Copyright (c) 2014 Leo Simberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BluetoothLeo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BluetoothLeo1
+//
+//  Created by Leo Simberg on 9/10/14.
+//  Copyright (c) 2014 Leo Simberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
